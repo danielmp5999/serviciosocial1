f10e78ef709c32f24923c6b371a7bc36a2c6d115
danielmp5999 <86995895+danielmp5999@users.noreply.github.com>
2022-09-27 20:29:31 -0500

    Main

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,6 +7,7 @@ class About extends Component {
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var sostenpic= "images/"+this.props.data.image2;
+      var mpa= "images/"+this.props.data.image3;
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
@@ -29,7 +30,7 @@ class About extends Component {
             <p>{bio}</p> 
             <div className="row">
                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
+                  <h2>Emisiones de CO2 en el mundo</h2>
                   <p className="address">
 						   <span>{name}</span><br />
 						   {/* <span>{street}<br />
@@ -42,7 +43,7 @@ class About extends Component {
                <div className="columns download">
                   <p>
                      {/* <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a> */}
-                     <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+                     <img className="item-wrap"  src={mpa} alt="Tim Baker Profile Pic" />
                   </p>
                </div>
             </div>
