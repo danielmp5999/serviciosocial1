823a4a3f7384ef104944b0661b9cf35f57ab429d
danielmp5999 <86995895+danielmp5999@users.noreply.github.com>
2022-10-03 01:56:35 -0500

    Resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,6 +5,7 @@ class Resume extends Component {
 
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
+      var metodo= "images/"+this.props.data.description32image1;
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
@@ -14,6 +15,26 @@ class Resume extends Component {
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
             <p>{work.description}</p>
+            <h1>{work.titledescription2}</h1>
+            <p>{work.description2}</p>
+            <h1>{work.titledescription3}</h1>
+            <p>{work.description3}</p>
+            <ul>
+              <li type="circle"><span>{work.description3lista1}</span></li>
+              <li type="circle"><span>{work.description3lista2}</span></li>
+              <li type="circle"><span>{work.description3lista3}</span></li>
+              <li type="circle"><span>{work.description3lista4}</span></li>
+            </ul>
+            <p>{work.description31}</p>
+            <ul>
+              <li type="square"><span>{work.description31lista1}</span></li>
+              <li type="square"><span>{work.description31lista2}</span></li>
+              <li type="square"><span>{work.description31lista3}</span></li>
+              <li type="square"><span>{work.description31lista4}</span></li>
+            </ul>
+            <p>{work.description32}</p>
+            <p><img className="item-wrap"  src={metodo} alt="Metodologia a seguir" /></p>
+
         </div>
       })
       var skills = this.props.data.skills.map(function(skills){
@@ -43,7 +64,7 @@ class Resume extends Component {
       <div className="row work">
 
          <div className="three columns header-col">
-            <h1><span>Work</span></h1>
+            <h1><span>Metodología</span></h1>
          </div>
 
          <div className="nine columns main-col">
